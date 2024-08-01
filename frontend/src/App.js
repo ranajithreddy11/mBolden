0dc92f08c301b41669cc9c21d5d32dd25d12ffb3
ranajithreddy11 <rlakkyre@asu.edu>
2024-08-01 14:37:47 -0500

    services and contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import LandingPage from './pages/LandingPage';
-import ServicesPage from './pages/ServicesPage';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+
 const App = () => {
     return (
         <Router>
             <Header />
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/services" element={<ServicesPage />} />
+                <Route path="/services" element={<Services/>} />
+                <Route path="/contact" element={<Contact/>} />
             </Routes>
             <Footer/>
         </Router>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/Header.css';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header className="header">
             <div className="header-container">
                 <div className="header-logo">
                     <Link to="/">mBolden</Link>
                 </div>
-                <nav className="header-nav">
-                    <Link to="/">ABOUT</Link>
-                    <Link to="/services">SERVICES</Link>
-                    <Link to="#insights">INSIGHTS</Link>
-                    <Link to="#capture">CAPTURE</Link>
-                    <Link to="#contact">CONTACT</Link>
+                <nav className={`header-nav ${isMenuOpen ? 'open' : ''}`}>
+                    <Link to="/" onClick={() => setIsMenuOpen(false)}>ABOUT</Link>
+                    <Link to="/services" onClick={() => setIsMenuOpen(false)}>SERVICES</Link>
+                    <Link to="/insights" onClick={() => setIsMenuOpen(false)}>INSIGHTS</Link>
+                    <Link to="/" onClick={() => setIsMenuOpen(false)}>CAPTURE</Link>
+                    <Link to="/contact" onClick={() => setIsMenuOpen(false)}>CONTACT</Link>
                 </nav>
+                <div className="hamburger" onClick={toggleMenu}>
+                    {isMenuOpen ? <FaTimes /> : <FaBars />}
+                </div>
             </div>
         </header>
     );
