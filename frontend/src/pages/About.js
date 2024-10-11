605402512756f28cc3cf8dd87a98d1d78630a017
kalyankotikalapudi <kalyankotikalapudi.99@gmail.com>
2024-10-10 17:46:43 -0700

    Added Dominc Chow

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,64 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import '../assets/styles/About.css';
 
 import MeetingRoom from '../assets/images/Aboutpage/MeetingRoom.png';
 import Onboarding from '../assets/images/Aboutpage/Onboarding.png';
 import People from '../assets/images/Aboutpage/People.png';
 import Timeline from '../assets/images/Aboutpage/Timeline.png';
-import vector1 from '../assets/images/Aboutpage/vector1.png';
-import vector2 from '../assets/images/Aboutpage/vector2.png';
-import vector3 from '../assets/images/Aboutpage/vector3.png';
-import vector4 from '../assets/images/Aboutpage/vector4.png';
-import suzanneimg from '../assets/images/Aboutpage/suzanneimg.png';
+
+import suzanneimg from '../assets/images/Aboutpage/suzanneimg.jpeg';
 import Instagram from '../assets/images/Aboutpage/Instagram.png';
 import Facebook from '../assets/images/Aboutpage/Facebook.png';
 import LinkedIn from '../assets/images/Aboutpage/LinkedIn.png';
-import polygoninside from '../assets/images/Aboutpage/polygoninside.png';
-
-import alanbresgi from '../assets/images/Aboutpage/alanbresgi.png';
-import hadikiani from '../assets/images/Aboutpage/hadikiani.png';
-import karenkumakanga from '../assets/images/Aboutpage/karenkumakanga.svg';
+import alanbresgi from '../assets/images/Aboutpage/alanbresgi.jpg';
+import dominic from '../assets/images/Aboutpage/dominic.jpg';
 
 
 
-import Slider from "react-slick";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 const About = () => {
-
-
     
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,   // Number of testimonials to show at once
-        slidesToScroll: 1, // Scroll one testimonial at a time
-        autoplay: true,    // Automatically move to next slide
-        autoplaySpeed: 3000,
-        arrows: false,     // Hide next/prev arrows
-
-        responsive: [
-            {
-                breakpoint: 1024, // iPads and smaller devices
-                settings: {
-                    slidesToShow: 1, // Show 2 testimonials on iPads
-                    slidesToScroll: 1,
-                    infinite: true,  // Keep looping
-                },
-            },
-            {
-                breakpoint: 768, // Mobile devices
-                settings: {
-                    slidesToShow: 1, // Show 1 testimonial on mobile
-                    slidesToScroll: 1,
-                    infinite: true,
-                },
-            },
-        ],
-    };
-
 
     const services = [
         {
@@ -72,11 +32,11 @@ const About = () => {
             title: 'Change, Communications, and Training'
         }, {
             icon: People,
-            title: 'Diversity, Equity, Inclusion and Belonging'
+            title: 'New Ways of Thinking and Working'
         }
     ];
 
-    const teamMember = {
+    const teamMember1 = {
         name: "Suzanne Knight",
         title: "CEO and Founder",
         imageUrl: suzanneimg, // Ensure this path is correct
@@ -97,15 +57,30 @@ const About = () => {
         ]
     };
 
-   
+
+
+    const teamMember2 = {
+        name: "Dominic Chow",
+        title: "Chief Operating Officer",
+        imageUrl: dominic // Ensure this path is correct
+    
+    };
+
+    const teamMember3 = {
+        name: "Alan Bresgi",
+        title: "Senior, Consulting Manager, Head of Communications",
+        imageUrl: alanbresgi // Ensure this path is correct
+    
+    };
+
+
+  
+
 
     return (
-
         <div className="about-main">
-
             <div className="about-container-1">
-
-                <h2>We are
+                <h2>We are &nbsp;
                     <span className="highlight">mBolden</span>
                 </h2>
 
@@ -129,108 +104,66 @@ const About = () => {
                             Our services range from aligning your corporate strategy with emerging
                             opportunities to creating a workplace that appeals to every generation.
                         </p>
-                        <button>Learn More</button>
+
+                        <div className="learnmore">
+                       <Link to="/services">Learn more</Link> 
+                       </div>
+
                     </div>
 
                     <div className="services-card-section">
-                        <div className="polygon">
-                            <div className="polygon-1">
-                                <div className="polygon-in">
-                                    <img src={polygoninside} alt="poly"/>
-                                    <div className="polygon-content">
-                                        <p>Pride and
-                                        </p>
-                                        <p>caring in all</p>
-                                        <p>we do</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div
-                            className="service-item-position-1"
-                            style={{
-                            backgroundImage: `url(${vector1})`
-                        }}>
-
-                            <img src={services[0].icon} alt={services[0].title} className="service-icon1"/>
+                        <div className="service-item-position-1">
+                            <img src={services[0].icon} alt={services[0].title} className="service-icon1" />
                             <h3 className='title1'>{services[0].title}</h3>
-
                         </div>
 
-                        <div
-                            className="service-item-position-2"
-                            style={{
-                            backgroundImage: `url(${vector2})`
-                        }}>
-
-                            <img src={services[1].icon} alt={services[1].title} className="service-icon2"/>
+                        <div className="service-item-position-2">
+                            <img src={services[1].icon} alt={services[1].title} className="service-icon2" />
                             <h3 className='title2'>{services[1].title}</h3>
-
                         </div>
 
-                        <div
-                            className="service-item-position-3"
-                            style={{
-                            backgroundImage: `url(${vector3})`
-                        }}>
-
-                            <img src={services[2].icon} alt={services[2].title} className="service-icon3"/>
+                        <div className="service-item-position-3">
+                            <img src={services[2].icon} alt={services[2].title} className="service-icon3" />
                             <h3 className='title3'>{services[2].title}</h3>
-
                         </div>
 
-                        <div
-                            className="service-item-position-4"
-                            style={{
-                            backgroundImage: `url(${vector4})`
-                        }}>
-
-                            <img src={services[3].icon} alt={services[3].title} className="service-icon4"/>
+                        <div className="service-item-position-4">
+                            <img src={services[3].icon} alt={services[3].title} className="service-icon4" />
                             <h3 className='title4'>{services[3].title}</h3>
-
                         </div>
-
                     </div>
                 </div>
-
             </div>
 
+            
             <div className="about-container-2">
                 <div className="container2-card1">
-
                     <div className="container2-profile">
                         <div className="container2-image-container">
                             <div className="container2-ellipse">
-
                                 <img
-                                    src={teamMember.imageUrl}
-                                    alt={teamMember.name}
-                                    className="container2-profile-image"/>
+                                    src={teamMember1.imageUrl}
+                                    alt={teamMember1.name}
+                                    className="container2-profile-image" />
                                 <div className="container2-profile-details">
-                                    <h2>{teamMember.name}</h2>
-                                    <h4>{teamMember.title}</h4>
+                                    <h2>{teamMember1.name}</h2>
+                                    <h4>{teamMember1.title}</h4>
                                 </div>
                             </div>
-
                         </div>
 
                         <div className="container2-social-links">
-                            {teamMember
-                                .socialLinks
-                                .map((link, index) => (
-                                    <a key={index} href={link.url} target="_blank" rel="noopener noreferrer">
-                                        <img src={link.icon} alt={link.name} className="container2-socialimage"/>
-                                    </a>
-                                ))}
+                            {teamMember1.socialLinks.map((link, index) => (
+                                <a key={index} href={link.url} target="_blank" rel="noopener noreferrer">
+                                    <img src={link.icon} alt={link.name} className="container2-socialimage" />
+                                </a>
+                            ))}
                         </div>
-
                     </div>
-
                 </div>
 
                 <div className="container2-card2">
-                    <h2>Meet Our Team</h2>
+                    <h2>Meet Our Leaders</h2>
                     <div className="container2-description">
                         <p>CEO of mBolden and DMZ's 2023 Woman of the Year, Suzanne Knight is a trusted
                             advisor for executives and inspirational leader for teams.</p>
@@ -243,52 +176,84 @@ const About = () => {
                             Finance and Strategy.</p>
                     </div>
                 </div>
-
             </div>
 
             <div className="about-container-3">
-
-
-            <div className="teammembers-section">
-                <h2>Our Team</h2>
-                <Slider {...settings} className="teammembers-carousel">
-                
-                    <div className="teammember">
-
-                        <img src={alanbresgi} alt="alanbresgi" className="teammember-logo" />
-
-                        <div className="teammember-info">
-                                     <h1 className="teammember-heading">Alan Bresgi</h1>
-                                      <h4>Senior Consulting Manager Head of Communications</h4>
+            <div className="container3-card1">
+                    <div className="container3-profile">
+                        <div className="container3-image-container">
+                            <div className="container3-ellipse">
+                                <img
+                                    src={teamMember2.imageUrl}
+                                    alt={teamMember2.name}
+                                    className="container3-profile-image" />
+                                <div className="container3-profile-details">
+                                    <h2>{teamMember2.name}</h2>
+                                    <h4>{teamMember2.title}</h4>
+                                </div>
+                            </div>
                         </div>
 
+                       
                     </div>
+                </div>
 
+                <div className="container3-card2">
                    
-                   
-                    <div className="teammember">
-                        <img src={hadikiani} alt="hadikiani" className="teammember-logo" />
-                        <div className="teammember-info">
-                        <h1 className="teammember-heading">Hadi Kiani</h1>
-                        <h4>Consultant</h4>
-                        </div>
+                    <div className="container3-description">
+                        <p>Alan brings over 18 years of experience leading corporate communications, internal communications, change management, employee engagement, DE&I, CSR/sustainability, and stakeholder relations.</p>
+                        <p> His passion lies in shaping workplace culture and fostering employee engagement. </p>
+                        <p>  From his roles at Takeda Pharmaceutical, British American Tobacco, Triumph International and Philip Morris International, he has consistently delivered results, leading communications, digital communication transformation, people engagement, change management, and innovation.</p>
+                        <p>Alan is fluent in Japanese and is a data-driven storyteller with an MBA from the Kellogg School of Management.</p>
                     </div>
-                    <div className="teammember">
-                        <img src={karenkumakanga} alt="karenkumakanga" className="teammember-logo" />
-                        <div className="teammember-info">
-                        <h1 className="teammember-heading">Karen Kumakanga</h1>
-                        <h4>Africa Market Lead Consultant</h4>
-                        </div>
-                    </div>
-                </Slider>
+                </div>
             </div>
 
-               </div>
+
+
+            <div className="about-container-3">
+            <div className="container3-card1">
+                    <div className="container3-profile">
+                        <div className="container3-image-container">
+                            <div className="container3-ellipse">
+                                <img
+                                    src={teamMember3.imageUrl}
+                                    alt={teamMember3.name}
+                                    className="container3-profile-image" />
+                                <div className="container3-profile-details">
+                                    <h2>{teamMember3.name}</h2>
+                                    <h4>{teamMember3.title}</h4>
+                                </div>
+                            </div>
+                        </div>
+
+                       
+                    </div>
+                </div>
+
+                <div className="container3-card2">
+                   
+                    <div className="container3-description">
+                        <p>Alan brings over 18 years of experience leading corporate communications, internal communications, change management, employee engagement, DE&I, CSR/sustainability, and stakeholder relations.</p>
+                        <p> His passion lies in shaping workplace culture and fostering employee engagement. </p>
+                        <p>  From his roles at Takeda Pharmaceutical, British American Tobacco, Triumph International and Philip Morris International, he has consistently delivered results, leading communications, digital communication transformation, people engagement, change management, and innovation.</p>
+                        <p>Alan is fluent in Japanese and is a data-driven storyteller with an MBA from the Kellogg School of Management.</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
 
         </div>
-
     );
-
 };
 
 export default About;
